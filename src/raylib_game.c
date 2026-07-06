e165394a59bd093592a4ce6ffed1b4d674a3c5f2
Jonas Malte Hinchely <jonas@hinchely.dk>
2026-07-06 21:15:18 +0200

    First person example

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -43,14 +43,24 @@ typedef enum {
 
 // TODO: Define your custom data types here
 
+struct GameState {
+    Camera *camera;
+} GameState;
+
 //----------------------------------------------------------------------------------
 // Global Variables Definition (local to this module)
 //----------------------------------------------------------------------------------
 static const int screenWidth = 720;
 static const int screenHeight = 720;
 
-static RenderTexture2D target = { 0 };  // Render texture to render our game
-static int frameCounter = 0;
+#define MAX_COLUMNS 20
+int frameCounter = 0;
+struct GameState* gs;
+int cameraMode = CAMERA_FIRST_PERSON;
+float heights[MAX_COLUMNS] = { 0 };
+Vector3 positions[MAX_COLUMNS] = { 0 };
+Color colors[MAX_COLUMNS] = { 0 };
+
 
 // TODO: Define global variables here, recommended to make them static
 
@@ -73,29 +83,49 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib gamejam template");
     
     // TODO: Load resources / Initialize variables at this point
+
+        // Define the camera to look into our 3d world (position, target, up vector)
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };    // Camera position
+    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 60.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+
+    gs = malloc(sizeof(struct GameState));
+    gs->camera = &camera;
+
+    // Generates some random columns
+
+
+    for (int i = 0; i < MAX_COLUMNS; i++)
+    {
+        heights[i] = (float)GetRandomValue(1, 12);
+        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i] / 2.0f, (float)GetRandomValue(-15, 15) };
+        colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
+    }
+
+    DisableCursor();                    // Limit cursor to relative movement inside the window
+
     
     // Render texture to draw, enables screen scaling
     // NOTE: If screen is scaled, mouse input should be scaled proportionally
-    target = LoadRenderTexture(screenWidth, screenHeight);
-    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
 #else
     SetTargetFPS(60);     // Set our game frames-per-second
     //--------------------------------------------------------------------------------------
+     
+    // SetExitKey(0);
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button
-    {
+    while (!WindowShouldClose()) {
         UpdateDrawFrame();
     }
 #endif
 
     // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadRenderTexture(target);
-    
+    //--------------------------------------------------------------------------------------    
     // TODO: Unload all loaded resources at this point
 
     CloseWindow();        // Close window and OpenGL context
@@ -104,10 +134,8 @@ int main(void)
     return 0;
 }
 
-//--------------------------------------------------------------------------------------------
-// Module Functions Definition
-//--------------------------------------------------------------------------------------------
-// Update and draw frame
+// https://github.com/raysan5/raylib/blob/master/examples/core/core_3d_camera_first_person.c
+
 void UpdateDrawFrame(void)
 {
     // Update
@@ -117,38 +145,151 @@ void UpdateDrawFrame(void)
     frameCounter++;
     //----------------------------------------------------------------------------------
 
+    Camera *camera = gs->camera;
+
     // Draw
     //----------------------------------------------------------------------------------
     // Render game screen to a texture, 
     // it could be useful for scaling or further shader postprocessing
-    BeginTextureMode(target);
+    {
         ClearBackground(RAYWHITE);
-        
-        // TODO: Draw your game screen here
+        // Update
+                //----------------------------------------------------------------------------------
+                // Switch camera mode
+        if (IsKeyPressed(KEY_ONE))
+        {
+            cameraMode = CAMERA_FREE;
+            camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
 
-        DrawRectangle(70, 90, 200, 200, BLACK);
-        DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
-        DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32, 90 + 200 - 40 - 24, 40, BLACK);
+        if (IsKeyPressed(KEY_TWO))
+        {
+            cameraMode = CAMERA_FIRST_PERSON;
+            camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
 
-        DrawText("6.x", 290, 90 - 26, 280, BLACK);
-        DrawText("GAMEJAM", 70, 90 + 210, 120, MAROON);
+        if (IsKeyPressed(KEY_THREE))
+        {
+            cameraMode = CAMERA_THIRD_PERSON;
+            camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
 
-        if ((frameCounter/20)%2) DrawText("are you ready?", 160, 500, 50, BLACK);
-        
-        DrawRectangleLinesEx((Rectangle){ 0, 0, screenWidth, screenHeight }, 16, BLACK);
-        
-    EndTextureMode();
-    
-    // Render to screen (main framebuffer)
-    BeginDrawing();
+        if (IsKeyPressed(KEY_FOUR))
+        {
+            cameraMode = CAMERA_ORBITAL;
+            camera->up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
+
+        // Switch camera projection
+        if (IsKeyPressed(KEY_P))
+        {
+            if (camera->projection == CAMERA_PERSPECTIVE)
+            {
+                // Create isometric view
+                cameraMode = CAMERA_THIRD_PERSON;
+                // Note: The target distance is related to the render distance in the orthographic projection
+                camera->position = (Vector3){ 0.0f, 2.0f, -100.0f };
+                camera->target = (Vector3){ 0.0f, 2.0f, 0.0f };
+                camera->up = (Vector3){ 0.0f, 1.0f, 0.0f };
+                camera->projection = CAMERA_ORTHOGRAPHIC;
+                camera->fovy = 20.0f; // near plane width in CAMERA_ORTHOGRAPHIC
+                CameraYaw(&camera, -135 * DEG2RAD, true);
+                CameraPitch(&camera, -45 * DEG2RAD, true, true, false);
+            }
+            else if (camera->projection == CAMERA_ORTHOGRAPHIC)
+            {
+                // Reset to default view
+                cameraMode = CAMERA_THIRD_PERSON;
+                camera->position = (Vector3){ 0.0f, 2.0f, 10.0f };
+                camera->target = (Vector3){ 0.0f, 2.0f, 0.0f };
+                camera->up = (Vector3){ 0.0f, 1.0f, 0.0f };
+                camera->projection = CAMERA_PERSPECTIVE;
+                camera->fovy = 60.0f;
+            }
+        }
+
+        // Update camera computes movement internally depending on the camera mode
+        // Some default standard keyboard/mouse inputs are hardcoded to simplify use
+        // For advanced camera controls, it's recommended to compute camera movement manually
+        UpdateCamera(camera, cameraMode);                  // Update camera
+        /*
+                // Camera PRO usage example (EXPERIMENTAL)
+                // This new camera function allows custom movement/rotation values to be directly provided
+                // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
+                UpdateCameraPro(&camera,
+                    (Vector3){
+                        (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
+                        (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,
+                        (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
+                        (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
+                        0.0f                                                // Move up-down
+                    },
+                    (Vector3){
+                        GetMouseDelta().x*0.05f,                            // Rotation: yaw
+                        GetMouseDelta().y*0.05f,                            // Rotation: pitch
+                        0.0f                                                // Rotation: roll
+                    },
+                    GetMouseWheelMove()*2.0f);                              // Move to target (zoom)
+        */
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
         ClearBackground(RAYWHITE);
-        
-        // Draw render texture to screen, scaled if required
-        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, 
-            (Rectangle){ 0, 0, (float)target.texture.width, (float)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
 
-        // TODO: Draw everything that requires to be drawn at this point, maybe UI?
+        BeginMode3D(*camera);
 
-    EndDrawing();
+        DrawPlane((Vector3) { 0.0f, 0.0f, 0.0f }, (Vector2) { 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
+        DrawCube((Vector3) { -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+        DrawCube((Vector3) { 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
+        DrawCube((Vector3) { 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
+
+        // Draw some cubes around
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+        }
+
+        // Draw player cube
+        if (cameraMode == CAMERA_THIRD_PERSON)
+        {
+            DrawCube(camera->target, 0.5f, 0.5f, 0.5f, PURPLE);
+            DrawCubeWires(camera->target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
+        }
+
+        EndMode3D();
+
+        // Draw info boxes
+        DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(5, 5, 330, 100, BLUE);
+
+        DrawText("Camera controls:", 15, 15, 10, BLACK);
+        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+        DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+        DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
+        DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
+        DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
+
+        DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(600, 5, 195, 100, BLUE);
+
+        DrawText("Camera status:", 610, 15, 10, BLACK);
+        DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
+            (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
+            (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
+            (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
+        DrawText(TextFormat("- Projection: %s", (camera->projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
+            (camera->projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
+        DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera->position.x, camera->position.y, camera->position.z), 610, 60, 10, BLACK);
+        DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera->target.x, camera->target.y, camera->target.z), 610, 75, 10, BLACK);
+        DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera->up.x, camera->up.y, camera->up.z), 610, 90, 10, BLACK);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+
+    }
     //----------------------------------------------------------------------------------  
 }
